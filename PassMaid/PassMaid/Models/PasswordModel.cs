f97ca109696cc95357680a3e225a243d3da9b9b8
hines-r <39021120+hines-r@users.noreply.github.com>
2018-12-12 13:17:48 -0700

    Created basic password model

---
+++ PassMaid/PassMaid/Models/PasswordModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassMaid.Models
+{
+    public class PasswordModel
+    {
+        public string Name { get; set; }
+        public string Website { get; set; }
+        public string Password { get; set; }
+    }
+}
--- PassMaid/PassMaid/ViewModels/ShellViewModel.cs
+++ PassMaid/PassMaid/ViewModels/ShellViewModel.cs
@@ -9,6 +9,44 @@ namespace PassMaid.ViewModels
 {
     public class ShellViewModel : Screen
     {
+        private string _name = "Password Name";
+        private string _website;
+        private string _password;
 
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                NotifyOfPropertyChange(() => Name);
+            }
+        }
+
+        public string Website
+        {
+            get { return _website; }
+            set
+            {
+                _website = value;
+                NotifyOfPropertyChange(() => Website);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => PasswordLength);
+            }
+        }
+
+        public int PasswordLength
+        {
+            get { return Password.Length; }
+        }
     }
 }
