9c3dba5ccc1181652ddebafd44695b8dc9bd365e
hines-r <39021120+hines-r@users.noreply.github.com>
2018-12-12 16:46:36 -0700

    Implemented AES-256 encryption

--- PassMaid/PassMaid/ViewModels/ShellViewModel.cs
+++ PassMaid/PassMaid/ViewModels/ShellViewModel.cs
@@ -3,15 +3,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Windows.Input;
 
 namespace PassMaid.ViewModels
 {
     public class ShellViewModel : Screen
     {
-        private string _name = "Password Name";
+        private string _name;
         private string _website;
         private string _password;
+        private string _cipher;
+
+        private AesCryptoServiceProvider aes;
+
+        public ShellViewModel()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            aes = new AesCryptoServiceProvider();
+
+            aes.BlockSize = 128;
+            aes.KeySize = 256;
+            aes.GenerateKey();
+            aes.GenerateIV();
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+        }
 
         public string Name
         {
@@ -47,6 +68,45 @@ namespace PassMaid.ViewModels
         public int PasswordLength
         {
             get { return Password.Length; }
+        }
+
+        public string Cipher
+        {
+            get { return _cipher; }
+            set
+            {
+                _cipher = value;
+                NotifyOfPropertyChange(() => Cipher);
+            }
+        }
+
+        public ICommand EncryptCommand => new RelayCommand(ExecuteEncrypt);
+
+        private void ExecuteEncrypt(object o)
+        {
+            if (Password != null)
+            {
+                ICryptoTransform transform = aes.CreateEncryptor();
+                byte[] encryptedBytes = transform.TransformFinalBlock(ASCIIEncoding.ASCII.GetBytes(Password), 0, Password.Length);
+                string encryptedText = Convert.ToBase64String(encryptedBytes);
+
+                Cipher = encryptedText;
+            }
+        }
+
+        public ICommand DecryptCommand => new RelayCommand(ExecuteDecrypt);
+
+        public void ExecuteDecrypt(object o)
+        {
+            if (Cipher != null)
+            {
+                ICryptoTransform transform = aes.CreateDecryptor();
+                byte[] encryptedBytes = Convert.FromBase64String(Cipher);
+                byte[] decryptedBytes = transform.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+                string decryptedText = ASCIIEncoding.ASCII.GetString(decryptedBytes);
+
+                Password = decryptedText;
+            }
         }
     }
 }
