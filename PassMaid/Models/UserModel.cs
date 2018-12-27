05324f9a79be3b933df49a500ca20648e7e5687a
hines-r <39021120+hines-r@users.noreply.github.com>
2018-12-26 17:04:13 -0700

    Created new user model

---
+++ PassMaid/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassMaid.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
