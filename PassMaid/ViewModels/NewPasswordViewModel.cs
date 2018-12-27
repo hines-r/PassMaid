dbf1811f7bf89763d5dc63c803b73638519ca238
hines-r <39021120+hines-r@users.noreply.github.com>
2018-12-26 22:47:20 -0700

    Users can now save passwords to the vault view model

--- PassMaid/ViewModels/NewPasswordViewModel.cs
+++ PassMaid/ViewModels/NewPasswordViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using PassMaid.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,75 @@ namespace PassMaid.ViewModels
 {
     public class NewPasswordViewModel : Screen
     {
+        private string _name;
+        private string _website;
+        private string _username;
+        private string _password;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                NotifyOfPropertyChange(() => Name);
+            }
+        }
+
+        public string Website
+        {
+            get { return _website; }
+            set
+            {
+                _website = value;
+                NotifyOfPropertyChange(() => Website);
+            }
+        }
+
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                NotifyOfPropertyChange(() => Username);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+            }
+        }
+
         public ICommand SaveCommand => new RelayCommand(ExecuteSave);
 
         public void ExecuteSave(object o)
         {
+            // TODO: Securely store password models
 
+            // User needs to input all fields
+            if (String.IsNullOrEmpty(Name) || String.IsNullOrEmpty(Website) || String.IsNullOrEmpty(Username) || String.IsNullOrEmpty(Password))
+            {
+                return;
+            }
+
+            PasswordModel newPassword = new PasswordModel()
+            {
+                Name = this.Name,
+                Website = this.Website,
+                Username = this.Username,
+                Password = this.Password                  
+            };
+
+            var parent = this.Parent as VaultTabViewModel;
+            var vault = parent.VaultScreens[0] as VaultViewModel;
+
+            vault.Passwords.Add(newPassword);
         }
 
         public ICommand CancelCommand => new RelayCommand(ExecuteCancel);
