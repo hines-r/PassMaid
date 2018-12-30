f27c092d2649034b5a6aa1e40364071bee3df69f
hines-r <39021120+hines-r@users.noreply.github.com>
2018-12-30 01:31:23 -0700

    Implemented database for password storage

---
+++ PassMaid/SqliteDataAcess.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using PassMaid.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassMaid
+{
+    public static class SqliteDataAcess
+    {
+        public static List<PasswordModel> LoadPasswords()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<PasswordModel>("SELECT * FROM Password", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        public static void SavePassword(PasswordModel password)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("INSERT INTO Password " +
+                    "(Name, Website, Username, Password) " +
+                    "VALUES (@Name, @Website, @Username, @Password)", password);
+            }
+        }
+
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
