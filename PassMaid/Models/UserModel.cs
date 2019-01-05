23ede3846661ce9e65a952a4413da2a81887a18b
hines-r <39021120+hines-r@users.noreply.github.com>
2019-01-04 18:44:26 -0700

    Add salt field to user table in the database

--- PassMaid/Models/UserModel.cs
+++ PassMaid/Models/UserModel.cs
@@ -11,5 +11,6 @@ namespace PassMaid.Models
         public int UserId { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string Salt { get; set; }
     }
 }
