9158e69389e32b5fe23278781ce049f562ae4cb8
hines-r <39021120+hines-r@users.noreply.github.com>
2019-01-05 14:39:52 -0700

    Add initialization vector to user table in database

--- PassMaid/Models/UserModel.cs
+++ PassMaid/Models/UserModel.cs
@@ -12,5 +12,6 @@ namespace PassMaid.Models
         public string Username { get; set; }
         public string Password { get; set; }
         public string Salt { get; set; }
+        public string IV { get; set; }
     }
 }
