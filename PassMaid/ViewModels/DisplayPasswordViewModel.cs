540892c9b0c9e375b7408a7fc3ffabe764b43b9b
hines-r <39021120+hines-r@users.noreply.github.com>
2019-01-09 19:29:18 -0700

    Implemented copy and toggle visibility buttons for display view

--- PassMaid/ViewModels/DisplayPasswordViewModel.cs
+++ PassMaid/ViewModels/DisplayPasswordViewModel.cs
@@ -5,20 +5,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace PassMaid.ViewModels
 {
     public class DisplayPasswordViewModel : PasswordScreen
     {
+        private const int HIDDEN_PASSWORD_LENGTH = 8;
+        private const string PASSWORD_CHAR = "•";
+
+        private bool IsVisible { get; set; }
+        private string HiddenPassword { get; set; }
+
         public DisplayPasswordViewModel(PasswordModel _selectedPassword, VaultViewModel _vaultViewModel) : base(_selectedPassword, _vaultViewModel)
         {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < HIDDEN_PASSWORD_LENGTH; i++)
+            {
+                sb.Append(PASSWORD_CHAR);
+            }
+
+            HiddenPassword = sb.ToString();
+
             if (SelectedPassword != null)
             {
                 this.Name = SelectedPassword.Name;
                 this.Website = SelectedPassword.Website;
                 this.Username = SelectedPassword.Username;
-                this.Password = SelectedPassword.Password;
+                this.Password = HiddenPassword;
             }
         }
 
@@ -29,6 +45,44 @@ namespace PassMaid.ViewModels
             if (SelectedPassword != null)
             {
                 VaultVM.PassScreenType = new EditPasswordViewModel(SelectedPassword, VaultVM);
+            }
+        }
+
+        public ICommand CopyUsernameCommand => new RelayCommand(ExecuteCopyUsername);
+
+        public void ExecuteCopyUsername(object o)
+        {
+            if (!String.IsNullOrEmpty(Username))
+            {
+                Clipboard.SetText(Username);
+            }
+        }
+
+        public ICommand CopyPasswordCommand => new RelayCommand(ExecuteCopyPassword);
+
+        public void ExecuteCopyPassword(object o)
+        {
+            string passwordToCopy = SelectedPassword.Password;
+
+            if (!String.IsNullOrEmpty(passwordToCopy))
+            {
+                Clipboard.SetText(passwordToCopy);
+            }
+        }
+
+        public ICommand ToggleVisibilityCommand => new RelayCommand(ExecuteToggleVisibility);
+
+        public void ExecuteToggleVisibility(object o)
+        {
+            IsVisible = !IsVisible; // Sets boolean opposite of what it was ex. false turns to true
+
+            if (IsVisible)
+            {
+                Password = SelectedPassword.Password;
+            }
+            else
+            {
+                Password = HiddenPassword;
             }
         }
     }
